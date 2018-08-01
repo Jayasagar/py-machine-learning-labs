f338c43dff3d297f30222fc303f9964bfa3930a6
jayasagar <jayasagar@gmail.com>
2018-08-01 06:02:15 +0200

    DAY 1: 2018-ASO Everyday commit: Euclidean distance vector norm calculation

---
+++ EuclideanDistance.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+# Day 1 of Everyday Code AI 2018
+# Ref: https://www.youtube.com/watch?v=hl3bQySs8sM
+# [3.3.7-Linear Algebra: Vector and Matrix Norms - YouTube](https://www.youtube.com/watch?v=tXCqr2UsbWQ&t=60s)
+
+plot1 = [2, 5]
+plot2 = [4, 5]
+
+
+euclidean_diatance = sqrt((plot1[0] - plot2[0]) **2 + (plot1[1] + plot2[1])**2)
+
+print euclidean_diatance
