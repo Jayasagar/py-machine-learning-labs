4738f2e033e3e85cc7b444e6166cc2cf8d481b43
Jayasagar <jayasagar@gmail.com>
2018-08-08 17:46:47 +0200

    DAY 8th 2018-ASO Everyday Commit: Basic dummy implementatyion HInge loss function

--- LossFunctions.py
+++ LossFunctions.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+
+# At fundamental level, Loss function is basic tool to calculate absolute difference between prediction made by teh algorithm and acua value.
 
 def CrossEntropy(yHat, y):
     if y == 1:
@@ -7,3 +10,9 @@ def CrossEntropy(yHat, y):
       return -math.log(1 - yHat)
 
 print(CrossEntropy(2, 1))
+
+# Used for classification
+def Hinge(yHat, y):
+    return np.max(0, 1 - yHat * y)
+
+print(Hinge(2, 1))
