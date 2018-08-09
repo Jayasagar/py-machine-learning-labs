50c2b021d17e00ba46229a8023f5599e6658e84c
Jayasagar <jayasagar@gmail.com>
2018-08-09 07:23:47 +0200

    DAY 9 2018-ASO Everyday Commit: Understood and implemented basic detailed steps of Root Mean Suared rror loss function

---
+++ RegressionLossFunctions.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# Regressive loss functions
+# Mean Square Error
+# Absolute Error measures
+# Smooth Absolute Error
+
+# Reference: https://stackoverflow.com/questions/17197492/root-mean-square-error-in-python
+
+# RMSE: (Root mean squared error), MSE: (Mean Squared Error) and RMS: (Root Mean Squared),
+
+def rootMeanSquaredError(predictions, actuals):
+    # Difference
+    difference = predictions - actuals
+
+    # Squared Differnece
+    difference_squared = difference ** 2
+
+    # Mean of the squared difference
+    mean_of_difference_squared = difference_squared.mean()
+
+    # Square root
+    square_root_of_mean_result = np.sqrt(mean_of_difference_squared)
+
+    return square_root_of_mean_result
+
+
+actuals = [0.000, 0.254, 0.998]
+predictions = [0.000, 0.166, 0.333]
+
+
+error = rootMeanSquaredError(np.array(predictions), np.array(actuals))
+
+print('Error', error)
