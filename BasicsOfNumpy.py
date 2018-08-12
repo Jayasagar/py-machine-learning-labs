da3b0bc895bb6782400ef990443257fe0a9abb53
Jayasagar <jayasagar@gmail.com>
2018-08-12 20:33:14 +0200

    DAY 12 2018-ASO Everyday Commit: Quick small learning on NumPy Slice array result is a refecrnce to orinal array, need to explicitly call .copy to get copy

--- BasicsOfNumpy.py
+++ BasicsOfNumpy.py
@@ -8,9 +8,9 @@ twoDimensional = np.random.randint(10, size=(3, 4))  # Two-dimensional array
 threeDimensional = np.random.randint(10, size=(3, 4, 5))  # Three-dimensional array
 
 
-print("threeDimensional ndim: ", threeDimensional.ndim)
-print("threeDimensional shape:", threeDimensional.shape)
-print("threeDimensional size: ", threeDimensional.size)
+# print("threeDimensional ndim: ", threeDimensional.ndim)
+# print("threeDimensional shape:", threeDimensional.shape)
+# print("threeDimensional size: ", threeDimensional.size)
 
 ## Array Slicing: Accessing Subarrays
 
@@ -31,7 +31,27 @@ print("threeDimensional size: ", threeDimensional.size)
 # print(twoDimensional[:2, :3])  # two rows, three columns
 # print(twoDimensional[:3, ::2])  # all rows, every other column
 
-print(twoDimensional)
-print(twoDimensional[::-1, ::-1]) ## Operating on 2 dimensions
-print(twoDimensional[::-1]) ## Only dealing with  first dimension
+# print(twoDimensional)
+# print(twoDimensional[::-1, ::-1]) ## Operating on 2 dimensions
+# print(twoDimensional[::-1]) ## Only dealing with  first dimension
 
+
+# Sub Array and No copy of SUB ARRAYS
+
+sub_array = threeDimensional[:2, :2, :2]
+# print(sub_array)
+
+#  Change any value will change original value
+sub_array[0][0][0] = 15
+
+# print sub_array
+
+# Creating copies of arrays
+sub_array_copy = threeDimensional[:2, :2, :2].copy()
+
+print sub_array_copy
+
+# Change any value, now it wont change
+sub_array_copy[0][0][0] = 19
+
+print sub_array
