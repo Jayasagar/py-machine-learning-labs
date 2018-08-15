bb6ffb0794610f67aacf585fd658fdc211fb32be
Jayasagar <jayasagar@gmail.com>
2018-08-15 07:16:21 +0200

    DAY 15 2018-ASO Everyday Commit: Generate normal distribution data using np.random.randn with help of Sigma and mu values

--- NormalDistribution.py
+++ NormalDistribution.py
@@ -3,7 +3,11 @@ import numpy as np
 
 # seed the random number generator
 np.random.seed(1)
-# generate univariate observations
+# Generate Normal distribution data
+# For random samples from N(\mu, \sigma^2), use:
+# sigma * np.random.randn(...) + mu
 data = 5 * np.random.randn(100) + 50
+
+print ('data', data)
 # summarize
 print('mean=%.3f stdv=%.3f' % (np.mean(data), np.std(data)))
