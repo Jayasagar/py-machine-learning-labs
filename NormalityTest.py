6c1d376c8edd0e309bec6bfa74aaea8627070471
Jayasagar <jayasagar@gmail.com>
2018-08-18 05:00:56 +0200

    DAY 18 ASO-2018 Normality Test using the Anderson-Darlin test from Scipy lib

--- NormalityTest.py
+++ NormalityTest.py
@@ -6,6 +6,7 @@ from numpy import std
 
 from scipy.stats import shapiro
 from scipy.stats import normaltest
+from scipy.stats import anderson
 
 from statsmodels.graphics.gofplots import qqplot
 
@@ -48,6 +49,7 @@ if p > alpha:
 else:
     print('Sample doesnot look like Gaussian(reject H0)')
 
+print('####### END of Shaipro #######')
 ####### D`Agostino's K2 Normality Test #######
 
 agostino_stat, agostino_p = normaltest(data)
@@ -59,3 +61,19 @@ if agostino_p > agostino_alpha:
     print ('Looks like Gaussian distribution(fail to reject H0)')
 else :
     print('Does not looks like Gaussian distribution(reject H0)')
+
+print('####### END of Agostino #######')
+####### Anderson- Darling Test ############
+
+anderson_result = anderson(data, dist='norm')
+print('Anderson Statistics: %.3f', anderson_result.statistic)
+
+for i in range(len(anderson_result.critical_values)):
+    significance_level, critical_values = anderson_result.significance_level[i], anderson_result.critical_values[i]
+    if anderson_result.statistic < critical_values:
+        print('%.3f: %.3f, looks like Gaussian distribution(Fail to reject H0)', significance_level, critical_values)
+
+    else:
+        print('%.3f: %.3f, NOT looks like Gaussian distribution(reject H0)', significance_level, critical_values)
+
+print('####### END of Anderson #######')
