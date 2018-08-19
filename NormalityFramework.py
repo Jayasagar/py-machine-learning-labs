085a978bf27b59ce25526fb37b35fe4daca5d369
Jayasagar <jayasagar@gmail.com>
2018-08-19 05:47:57 +0200

    DAY 19 ASO-2018 Plot QQ and histo graps to see is the data normal or not, to be honest, not clear :) it is only done for popularity column

--- NormalityFramework.py
+++ NormalityFramework.py
@@ -1,5 +1,8 @@
 import matplotlib
 matplotlib.use('TkAgg')
+from matplotlib import pyplot
+
+from statsmodels.graphics.gofplots import qqplot
 
 from CSVService import CSVService
 
@@ -15,8 +18,20 @@ class NormalityFrameowrk:
         # print('Maximum value:', dataFrame.max())
         print('4th Row:', dataFrame.loc[4])
 
+        # Type: Pandas Series
         popularity  = dataFrame['popularity']
         print('Max', type(popularity), popularity.max())
+
+        ### Hsitogram
+        # pyplot.hist(popularity)
+        # pyplot.show()
+
+        #### QQ Plot
+        # qqplot(popularity, line='s')
+        # pyplot.show()
+
+
+        
 
 normality_framework = NormalityFrameowrk()
 normality_framework.main()
