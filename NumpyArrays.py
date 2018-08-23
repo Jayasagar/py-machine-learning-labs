122fb2387b1440b395b4d347fb506b32759cb1f5
Jayasagar <jayasagar@gmail.com>
2018-08-23 21:20:23 +0200

    DAY 23 ASO-2018 practice numpy arrays operaion using grades array example

---
+++ NumpyArrays.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# rerefence: https://www.pythonlikeyoumeanit.com/Module3_IntroducingNumpy/AccessingDataAlongMultipleDimensions.html
+
+# using a 2-dimensional array to store the grades
+grades = np.array([
+    [93,  95],
+    [84, 100],
+    [99,  87]
+])
+
+    
