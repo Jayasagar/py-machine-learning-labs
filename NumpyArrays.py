832f6ea2f22b2c5364e56a05c48730c471341761
Jayasagar <jayasagar@gmail.com>
2018-08-29 21:07:28 +0200

    DAY 29 ASO-2018 NumPy provides a mechanism for performing mathematical operations on arrays of unequal shapes

--- NumpyArrays.py
+++ NumpyArrays.py
@@ -28,3 +28,11 @@ d3_array = np.array([[[0, 1],
 print ('No of Dimensions', d3_array.ndim)
 
 print('d3_array[1]:', d3_array[1]) # which recall is shorthand for d3_array[1, :, :]
+
+x = np.array([[-0. , -0.1, -0.2, -0.3],
+[-0.4, -0.5, -0.6, -0.7],
+[-0.8, -0.9, -1. , -1.1]])
+
+y = np.array([1, 2, 3, 4])
+
+print('NumPy provides a mechanism for performing mathematical operations on arrays of unequal shapes: ', x * y)
