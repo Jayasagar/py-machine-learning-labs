8c75d4e2cdacfcbeb8155ea1522113350d263861
Jayasagar <jayasagar@gmail.com>
2018-09-02 04:49:11 +0200

    ASO-2018: We can perform paired samples as a two samples and do how much independent they, normally distributed is same? and same variance, this is all what we can get inights from this data

--- ParametricStatisticalHypothesisTests.py
+++ ParametricStatisticalHypothesisTests.py
@@ -1,4 +1,5 @@
 from scipy.stats import ttest_ind
+from scipy.stats import ttest_rel
 
 from CSVService import CSVService
 
@@ -6,6 +7,12 @@ csv = CSVService()
 
 dataFrame = csv.read_from_csv()
 
+# Tests whether the means of two INDEPENDENT samples are significantly different
 stat, p = ttest_ind(dataFrame['vote_count'], dataFrame['vote_average'])
 
 print('Stat:', stat, "p:", p)
+
+# Tests whether the means of two PAIRED samples are significantly different.
+stat_1, p_1 = ttest_rel(dataFrame['vote_count'], dataFrame['vote_average'])
+
+print('Paired Student test: ', stat_1, p_1)
