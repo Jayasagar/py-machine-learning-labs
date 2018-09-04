b39e68e4a0645e7298fa098f4c75822b98fdf42e
Jayasagar <jayasagar@gmail.com>
2018-09-04 04:25:58 +0200

    ASO-2018 Nonparametric Statistical Hypothesis Tests experiment

--- ParametricStatisticalHypothesisTests.py
+++ ParametricStatisticalHypothesisTests.py
@@ -1,6 +1,7 @@
 from scipy.stats import ttest_ind
 from scipy.stats import ttest_rel
 from scipy.stats import f_oneway
+from scipy.stats import mannwhitneyu
 
 from CSVService import CSVService
 
@@ -17,8 +18,15 @@ print('Stat:', stat, "p:", p)
 stat_1, p_1 = ttest_rel(dataFrame['vote_count'], dataFrame['vote_average'])
 
 print('Paired Student test: ', stat_1, p_1)
-    
+
 # Analysis of Variance Test (ANOVA)
 stat_2, p_2 = f_oneway(dataFrame['vote_count'], dataFrame['vote_average'])
 
 print('Analysis of Variance Test (ANOVA): ', stat_2, p_2)
+
+######## Nonparametric Statistical Hypothesis Tests
+
+# Mann-Whitney U Test
+stat_3, p_3 = mannwhitneyu(dataFrame['vote_count'], dataFrame['vote_average'])
+
+print('Mann-Whitney U Test: ', stat_3, p_3)
