955035cb8882b1f4c3fa89713f4dc5ba6f2c1bad
Jayasagar <jayasagar@gmail.com>
2018-10-03 08:24:23 +0200

    Now system info shows up including num of cores

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
-
+import psutil
 
 # Check the number of cores and memory usage
 num_cores = mp.cpu_count()
-print("This kernel has ",num_cores,"cores and you can find the information regarding the memory usage:",psutil.virtual_memory())
+print("This kernel has: ", num_cores, 
+"cores and you can find the information regarding the memory usage:", psutil.virtual_memory())
